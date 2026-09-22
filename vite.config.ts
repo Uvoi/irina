4cf83df8ba4661892483ff77f33e2fb8b5247e48
uvoi <iovu_dmtr@yahoo.com>
2026-09-22 14:40:22 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/irina/',
   resolve: {
     alias: {
       // Привязываем символ @ к папке src
